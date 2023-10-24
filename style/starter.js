b9a2d5d976c77d9e75bfc74577c328cb5ce7b978
Rian Watson <watson02@allegheny.edu>
2023-10-24 18:09:03 -0400

    side bar

---
+++ style/starter.js
@@ -0,0 +1,13 @@
+//Functions on the starter page
+
+//side bar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    }
+// end of side bar
